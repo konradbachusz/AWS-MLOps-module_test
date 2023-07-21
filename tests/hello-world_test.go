5721c5a85f54c46cd0c143e3a46ed1b5dd2cc279
Anisha Das <anisha.das@credera.co.uk>
2023-07-21 18:21:08 +0100

    created 2nd func for terratest

--- tests/hello-world_test.go
+++ tests/hello-world_test.go
@@ -10,7 +10,7 @@ import (
 func TestTerraformHelloWorldExample(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
-		TerraformDir: "../", 
+		TerraformDir: "../tests-examples/", 
 	})
 
 	// Clean up resources 
@@ -22,4 +22,18 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	// Run `terraform output` to get the values of output variables and check they have the expected values.
 	output := terraform.Output(t, terraformOptions, "hello_world")
 	assert.Equal(t, "Hello, World!", output)
+}
+
+func Test2(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../", 
+	})
+
+	// Clean up resources 
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
 }
