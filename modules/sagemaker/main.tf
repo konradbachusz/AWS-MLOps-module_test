620d4603cb9f3af01fdf9e68b06118c90ed80de5
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2023-08-11 16:09:14 +0100

    image fix test

--- modules/sagemaker/main.tf
+++ modules/sagemaker/main.tf
@@ -8,10 +8,11 @@ resource "aws_sagemaker_model" "my_model" {
   execution_role_arn = module.iam.sagemaker_role_arn
 
   primary_container {
-    image = data.aws_sagemaker_prebuilt_ecr_image.image.registry_path
+    image = var.sagemaker_image_repository_name
   }
 }
 
-data "aws_sagemaker_prebuilt_ecr_image" "image" {
-  repository_name = var.sagemaker_image_repository_name
-}
+#TODO Fix
+# data "aws_sagemaker_prebuilt_ecr_image" "image" {
+#   repository_name = var.sagemaker_image_repository_name
+# }
