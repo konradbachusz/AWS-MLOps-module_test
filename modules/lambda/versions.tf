0c9766ec22bf22d78e9f9136dc537431225a841f
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2023-08-18 15:01:45 +0100

    added versions to Lambda

---
+++ modules/lambda/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    archive = "~> 2.4"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.4"
+    }
+  }
+}
