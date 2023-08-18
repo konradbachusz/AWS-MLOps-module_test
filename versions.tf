056be6e77b0cc1804a3cf645b5fee97c55871868
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2023-08-18 15:19:53 +0100

    Updated the archive provider

--- versions.tf
+++ versions.tf
@@ -2,7 +2,10 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    archive = ">= 2.4"
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.0"
