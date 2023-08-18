07b17ea995e3722b52b858186aaca6d565a48ca1
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2023-08-18 15:14:22 +0100

    version updates

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    archive = "~> 2.4"
+    archive = ">= 2.4"
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.0"
