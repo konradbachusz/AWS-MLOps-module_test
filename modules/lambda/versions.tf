70431944586240375ef97c70a84293e636d6d8fd
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2023-08-18 15:06:40 +0100

    simplified required_providers in Lambda

--- modules/lambda/versions.tf
+++ modules/lambda/versions.tf
@@ -3,14 +3,5 @@ terraform {
 
   required_providers {
     archive = "~> 2.4"
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.4"
-    }
   }
 }
