9f6c532e8fb3e7a41ebf4f65a9857318b38e24d6
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2023-09-29 15:19:20 +0100

    added sagemaker domain

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ module "sagemaker" {
   tags                            = var.tags
   sagemaker_execution_role_arn    = module.iam.sagemaker_role_arn
   endpoint_instance_type          = var.endpoint_instance_type
+  vpc_id                          = var.vpc_id
+  subnet_ids                      = var.subnet_ids
 }
 
 module "iam" {
