7689864784ba7efdf2828124998d41a3916bf231
olanrewajubakare <bakarevo@gmail.com>
2023-10-09 15:05:53 +0100

    removed duplicated  variable name

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,9 +2,12 @@ variable "tags" {
   description = "Tags applied to your resources"
 }
 
+variable "model_name" {
+  description = "Name of the Sagemaker model"
+  type        = string
+}
+
 
 variable "mlops_s3_bucket" {
 }
 
-variable "model_name" {
-}
