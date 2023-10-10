e78e9667e6117e85b1b3cd3683daeb56bd95ef37
olanrewajubakare <bakarevo@gmail.com>
2023-10-10 10:10:33 +0100

    removed the dummy sagemaker model

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ module "s3" {
 
 module "sagemaker" {
   source                          = "./modules/sagemaker"
-  model_name                      = var.model_name
-  sagemaker_image_repository_name = var.sagemaker_image_repository_name
+  # model_name                      = var.model_name
+  # sagemaker_image_repository_name = var.sagemaker_image_repository_name
   tags                            = var.tags
   sagemaker_execution_role_arn    = module.iam.sagemaker_role_arn
-  endpoint_instance_type          = var.endpoint_instance_type
-  vpc_id                          = var.vpc_id
-  subnet_ids                      = var.subnet_ids
+  # endpoint_instance_type          = var.endpoint_instance_type
+  # vpc_id                          = var.vpc_id
+  # subnet_ids                      = var.subnet_ids
   model_target                    = var.model_target
   model_features                  = var.model_features
   s3_bucket                       = var.s3_bucket
