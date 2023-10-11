6299e060461c1db1d377bed2c240db6e477309d7
olanrewajubakare <bakarevo@gmail.com>
2023-10-11 10:08:28 +0100

    removed count

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,12 +1,18 @@
 #Creating an S3 bucket to heep the trained model
 
-variable "bucket_names" {
-  default = ["${var.model_name}-model", "${var.model_name}-config-bucket", var.mlops_s3_bucket]
+locals {
+  file_path = "${path.module}/../../mlops_ml_models"
+  files_to_upload = concat(
+    tolist(fileset(local.file_path, "*.ipynb")),
+    tolist(fileset(local.file_path, "*.py"))
+  )
+  bucket_names = list("${var.model_name}-model", "${var.model_name}-config-bucket", var.mlops_s3_bucket)
 }
 
+
 resource "aws_s3_bucket" "model_buckets" {
-  count  = length(var.bucket_names)
-  bucket = var.bucket_names[count.index]
+  count  = length(local.file_path[count.index])
+  bucket = local.file_path[count.index]
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_access_block" {
@@ -17,18 +23,11 @@ resource "aws_s3_bucket_public_access_block" "s3_access_block" {
 }
 
 
-locals {
-  file_path = "${path.module}/../../mlops_ml_models"
-  files_to_upload = concat(
-    tolist(fileset(local.file_path, "*.ipynb")),
-    tolist(fileset(local.file_path, "*.py"))
-  )
-}
 
 
 resource "aws_s3_bucket_object" "s3_files" {
   for_each = toset(local.files_to_upload)
-  count  = aws_s3_bucket.model_buckets[2].id == null ? 0 : 2
+  # count  = aws_s3_bucket.model_buckets[2].id == null ? 0 : 2
   bucket = aws_s3_bucket.model_buckets[2].id 
   key      = each.value
   source   = "${local.file_path}/${each.value}"
