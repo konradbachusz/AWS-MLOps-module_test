992a41921aa2ae304625607cb7c4fdb034d11bb5
olanrewajubakare <bakarevo@gmail.com>
2023-10-18 09:04:07 +0100

    added force delete to ECR

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_name" {
-    description = "The ECR name"
-  value = try(aws_ecr_repository.mlops_pycaret_repo.name, "")
+  description = "The ECR name"
+  value       = try(aws_ecr_repository.mlops_pycaret_repo.name, "")
 }
