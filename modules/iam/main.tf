f53a330524d4d0c753dfe4855aa748cca6f7a9ea
Daniel Lenton <daniel.lenton@credera.co.uk>
2023-10-20 17:22:44 +0100

    test: adding kms permissions to decrypt bucket object

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -112,11 +112,37 @@ EOF
 #   tags   = var.tags
 # }
 
+resource "aws_iam_policy" "kms" {
+  name   = "${var.model_name}-kms-policy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+          "Sid": "AllowAccessToKey",
+          "Effect": "Allow",
+          "Action": [
+            "kms:Decrypt", 
+            "kms:GenerateDataKey"
+          ],
+          "Resource": "arn:aws:kms:${var.region}:${var.account_id}:key/*"
+        }
+    ]
+}
+EOF
+  tags   = var.tags
+}
+
 
 # resource "aws_iam_role_policy_attachment" "sagemaker_policy_attachment" {
 #   role       = aws_iam_role.sagemaker_role.name
 #   policy_arn = aws_iam_policy.sagemaker_policy.arn
 # }
+
+resource "aws_iam_role_policy_attachment" "kms" {
+  role       = aws_iam_role.sagemaker_role.name
+  policy_arn = aws_iam_policy.kms.arn
+}
 
 resource "aws_iam_role_policy_attachment" "sagemaker_policy_attachment" {
   role       = aws_iam_role.sagemaker_role.name
