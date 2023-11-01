d1cd2ea25b19687ecdda955601568d7d8216a9a3
olanrewajubakare <bakarevo@gmail.com>
2023-11-01 11:24:08 +0000

    made image tags immutable

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "mlops_pycaret_repo" {
   name         = var.pycaret_ecr_name
   force_delete = true
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
