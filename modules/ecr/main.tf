9f5196e88fa8bc5b03e46ffafde4f717316dedef
olanrewajubakare <bakarevo@gmail.com>
2023-11-01 15:11:27 +0000

    changed the mutability tag to mutable

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -5,7 +5,7 @@ resource "aws_kms_key" "ecr_kms" {
 resource "aws_ecr_repository" "mlops_pycaret_repo" {
   name                 = var.pycaret_ecr_name
   force_delete         = true
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
