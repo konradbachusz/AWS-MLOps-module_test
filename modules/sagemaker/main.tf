be1367e7c8773620f3eb6ce0d70c61092110d5bc
Kong Nopwattanapong <kong.nopwattanapong@credera.co.uk>
2023-11-02 16:16:50 +0000

    feat: endpoint name + model_name

--- modules/sagemaker/main.tf
+++ modules/sagemaker/main.tf
@@ -21,6 +21,8 @@ resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "sagemaker_li
        echo "data_location_s3=${var.data_location_s3}" > /home/ec2-user/SageMaker/.env
        echo "target=${var.model_target_variable}" >> /home/ec2-user/SageMaker/.env
        echo "algorithm_choice=${var.algorithm_choice}" >> /home/ec2-user/SageMaker/.env
+       echo "endpoint_name=${var.endpoint_name}" >> /home/ec2-user/SageMaker/.env
+       echo "model_name=${var.model_name}" >> /home/ec2-user/SageMaker/.env
      EOL
   )
 }
