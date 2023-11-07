36257efbf49a115308ecc077db63eeb3d0de55d9
olanrewajubakare <bakarevo@gmail.com>
2023-11-07 11:42:15 +0000

    fixed the script to save model to s3

--- mlops_ml_models/save_model_to_s3.py
+++ mlops_ml_models/save_model_to_s3.py
@@ -12,8 +12,7 @@ def save_model_to_s3(
         bucket_name (str): name of the bucket you are saving the file
         final_model (object): the ml model you are trying to save
     """
-
-    with tarfile.open("final_best_model.tar.gz", "w:gz") as tar:
+    with tarfile.open(f"{trained_model_name}.tar.gz", "w:gz") as tar:
         tar.add(f"{trained_model_name}.pkl")
 
     s3 = boto3.client("s3")
