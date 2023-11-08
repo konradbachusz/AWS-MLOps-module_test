cb76f43def59d5acb21883a7914cb2cb10460310
olanrewajubakare <bakarevo@gmail.com>
2023-11-08 09:19:29 +0000

    trying to output the ecr repo name directly

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_name" {
   description = "The ECR name"
-  value       = aws_ecr_repository.mlops_pycaret_repo.name
+  value       = var.pycaret_ecr_name
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "sagemaker_model_name" {
   description = "The name of the model"
   value       = module.sagemaker.model_name
 }
+
+output "ecr_name" {
+  description = "the name of the ecr repo"
+  value = module.ecr.ecr_name
+}
