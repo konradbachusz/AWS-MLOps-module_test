97ceeff3e59fbd5926dcb80411cf18a069be9b3b
Kong Nopwattanapong <kong.nopwattanapong@credera.co.uk>
2023-11-10 13:39:47 +0000

    feat: revert prediction py

--- pycaret_image_files/prediction_script.py
+++ pycaret_image_files/prediction_script.py
@@ -6,7 +6,7 @@ from pycaret.regression import load_model
 import flask
 from flask import Flask, request
 import pandas as pd
-
+import os
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,12 +14,15 @@ logging.basicConfig(level=logging.DEBUG)
 # Instantiate Flask app
 app = Flask(__name__)
 
+MODEL_NAME = os.getenv('MODEL_NAME')
+
 # Define the model path
 # When you configure the model, you will need to specify the S3 location of
 # your model artifacts.
 # Sagemaker will automatically download, decompress and store the model's
 # weights in the /opt/ml/model folder.
-MODEL_PATH = "/opt/ml/model/final_best_model"
+MODEL_PATH = f"/opt/ml/model/{MODEL_NAME}"
+logging.info(MODEL_PATH)
 
 # Load the model from the specified path
 model = load_model(MODEL_PATH)
@@ -36,16 +39,14 @@ def ping():
 def predict():
     # Get data from the POST request
     data = request.get_data().decode("utf-8")
-    print(data)
 
     # Convert the data into a Pandas DataFrame
     df = pd.read_json(data, orient="split")
-    print(df)
+    logging.info(df)
 
     # Make predictions using the loaded model
     prediction = model.predict(df)
     logging.debug(f"Prediction: {prediction}")
-    print(prediction)
 
     # Return the prediction results as JSON
     return json.dumps(prediction.tolist())
