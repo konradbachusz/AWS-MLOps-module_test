aa4877b903768a197f8e48e3bb2560372a11c264
Kong Nopwattanapong <kong.nopwattanapong@credera.co.uk>
2023-11-13 13:46:49 +0000

    update save to s3 function

--- mlops_ml_models/save_model_to_s3.py
+++ mlops_ml_models/save_model_to_s3.py
@@ -3,14 +3,13 @@ import boto3
 
 
 def save_model_to_s3(
-    trained_model_name: str, bucket_name: str, final_model: object
+    trained_model_name: str, bucket_name: str
 ) -> None:
     """This saves the pkl model is s3 bucket
 
     Args:
         trained_model_name (str): name of the file you are trying to save in s3
         bucket_name (str): name of the bucket you are saving the file
-        final_model (object): the ml model you are trying to save
     """
     with tarfile.open(f"{trained_model_name}.tar.gz", "w:gz") as tar:
         tar.add(f"{trained_model_name}.pkl")
