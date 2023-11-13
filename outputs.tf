f6e4cf2c10928edf5b8dcf0c1a42479cac06bbb1
Kong Nopwattanapong <kong.nopwattanapong@credera.co.uk>
2023-11-13 13:54:45 +0000

    feat: output fix

--- outputs.tf
+++ outputs.tf
@@ -4,12 +4,12 @@ output "sagemaker_model_name" {
 }
 
 output "ecr_name" {
-  description = "the name of the ecr repo"
+  description = "The name of the ecr repo"
   value       = module.ecr.ecr_name
 }
 
 output "sagemaker_endpoint_name" {
-  description = "the sagemaker endpoint and configuration name"
+  description = "Model endpoint name"
   value       = module.sagemaker.sagemaker_endpoint_name
 }
 
