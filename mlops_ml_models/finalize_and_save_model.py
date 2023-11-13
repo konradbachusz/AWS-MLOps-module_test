12d6d983cfadb43266be2e4d6b99c861964d7bfa
Kong Nopwattanapong <kong.nopwattanapong@credera.co.uk>
2023-11-13 14:19:25 +0000

    feat: fix flake

--- mlops_ml_models/finalize_and_save_model.py
+++ mlops_ml_models/finalize_and_save_model.py
@@ -5,7 +5,7 @@ def finalize_and_save_model(algorithm_choice: str, bestModel: str, model_name: s
     """
     Finalizes the best model obtained from PyCaret and saves it locally.
 
-    This function finalizes the model by training it on the entire dataset and then saves it to a file. 
+    This function finalizes the model by training it on the entire dataset and then saves it to a file.
     It utilizes dynamic import to load the specific PyCaret submodule based on the algorithm choice.
 
     Args:
