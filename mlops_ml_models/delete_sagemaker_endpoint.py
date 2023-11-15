d8c8ff2999206738302f957494c2a55e4aeb7861
olanrewajubakare <bakarevo@gmail.com>
2023-11-15 13:21:26 +0000

    refactored some code

--- mlops_ml_models/delete_sagemaker_endpoint.py
+++ mlops_ml_models/delete_sagemaker_endpoint.py
@@ -3,27 +3,35 @@ import boto3
 
 def delete_sagemaker_endpoint(endpoint_name: str, region_name: str) -> None:
     """
-    Deletes the specified SageMaker endpoint and its configuration after user confirmation.
+    Deletes the specified SageMaker endpoint and its configuration after user
+    confirmation.
 
-    This function first asks the user to confirm the deletion. If the user confirms,
-    it proceeds to delete the SageMaker endpoint and its associated configuration.
+    This function first asks the user to confirm the deletion. If the user
+    confirms,
+    it proceeds to delete the SageMaker endpoint and its associated
+    configuration.
 
     Args:
         endpoint_name (str): The name of the SageMaker endpoint to delete.
-        region_name (str): The AWS region where the SageMaker endpoint is located.
+        region_name (str): The AWS region where the SageMaker endpoint is
+        located.
     """
     # Create SageMaker client
-    sagemaker_client = boto3.client('sagemaker', region_name=region_name)
+    sagemaker_client = boto3.client("sagemaker", region_name=region_name)
 
     # Ask for user confirmation
-    confirmation = input(f"Are you sure you want to delete the endpoint '{endpoint_name}'? Type 'Yes' to confirm: ")
+    confirmation = input(
+        f"Are you sure you want to delete the endpoint '{endpoint_name}'? Type 'Yes' to confirm: "
+    )
 
-    if confirmation.lower() == 'yes':
+    if confirmation.lower() == "yes":
         # Delete endpoint
         sagemaker_client.delete_endpoint(EndpointName=endpoint_name)
 
         # Delete endpoint configuration
-        sagemaker_client.delete_endpoint_config(EndpointConfigName=endpoint_name)
+        sagemaker_client.delete_endpoint_config(
+            EndpointConfigName=endpoint_name
+            )
 
         print(f"Endpoint '{endpoint_name}' and its configuration have been deleted.")
     else:
