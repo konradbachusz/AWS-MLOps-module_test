9a91c3212cafba56887c06bd1ba5bde9088d9e99
olanrewajubakare <bakarevo@gmail.com>
2024-01-09 15:15:33 +0000

    giving sagemaker the ability to create bucket (this will be reviewed later)

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -54,7 +54,8 @@ resource "aws_iam_policy" "sagemaker_policy" {
                 "s3:GetObject",
                 "s3:PutObject",
                 "s3:DeleteObject",
-                "s3:ListBucket"
+                "s3:ListBucket", 
+                "s3:CreateBucket"
             ],
             "Resource": [
                 "arn:aws:s3:::streaming-data-platform-ml-data",
